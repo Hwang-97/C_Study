388d180bc50549da92c1a435110de4363e1daed3
Hwang-97 <qqq4472@gmail.com>
2022-02-18 17:40:36 +0900

    2022-02-18 기초 공부

---
+++ NadoCoding/NadoCoding/NadoCoding.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+
+int main(void)
+{
+	/*int age = 12;
+	printf("%d\n",age);
+	age = 13;
+	printf("나이는 %d\n", age);
+	printf("Hello World\n");
+	return 0;
+	*/
+	/*
+	float f = 3.14f;
+
+	double d = 5.28;
+
+	printf("%.2f",f);
+
+	printf("%.1f",d);
+	*/
+	/*
+	const int YEAR = 2000;
+
+	printf("%d",YEAR);
+	*/
+	/*
+	int add = 3 + 7;
+
+	printf("%d\n",add);
+
+	int a = 4;
+	int b = 7;
+
+	printf("%d\n",a+b);
+	printf("%d\n",3+5);
+	*/
+	/*
+	int i = 0;
+
+	scanf_s("%d",&i);
+
+	printf("%d",i);
+
+	printf("3개의 정수를 입력하세요.\n");
+
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	scanf_s("%d %d %d",&a,&b,&c);
+
+	printf("%d %d %d",a,b,c);
+	*/
+	/*
+	char str[256];
+	scanf_s("%s",str,sizeof(str));
+	printf("%s\n",str);
+
+	return 0;
+
+	*/
+	/*
+	char name[256];
+
+	printf("이름이 뭐예요?");
+	scanf_s("%s",&name,sizeof(name));
+
+	int age;
+
+	printf("몇살이죠?");
+	scanf_s("%d", &age);
+
+	float weight;
+
+	printf("몸무게는 몇 kg 인가요?");
+	scanf_s("%f", &weight);
+
+	double height;
+
+	printf("키는 몇 cm 인가요?");
+	scanf_s("%lf", &height);
+
+	char what[256];
+
+	printf("무슨 범죄를 저질렀어요?");
+	scanf_s("%s",&what,sizeof(what));
+
+
+	printf("\n\n -- 범죄자 정보 -- \n\n");
+
+	printf("이름		:%s\n",	name);
+	printf("나이		:%d\n", age);
+	printf("키		:%f\n", height);
+	printf("몸무게	:%f\n", weight);
+	printf("무슨범죄	:%s\n", what);
+
+	*/
+
+	//printf("Hello World\n");
+	
+	for (int i = 0; i < 10; i++) {
+		printf("%d\n", i + 1);
+	}
+
+
+
+
+
+	return 0;
+}
