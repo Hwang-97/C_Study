1542e767dfeaee5df41d5dfccead3f54a36fbb1b
qqq4472 <qqq4472@gmail.com>
2022-06-14 23:32:04 +0900

    Algorithm complete

---
+++ Algorethm/HyunWoo_20220614_1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void main(int argc , char * argv[]){
+
+	int i;
+	char* text;
+	text = (char*)malloc(50*sizeof(char)); 
+	text = argv[1];
+	// A ~ Z 65 ~ 90
+	// a ~ z 97 ~ 122
+	if((int)text[0]>=97 && (int)text[0]<=122){
+		text[0] =(char)((int)text[0]-32);
+	}
+	if((int)text[strlen(text)]>=97 && (int)text[strlen(text)]<=122){
+		text[strlen(text)] =(char)((int)text[strlen(text)]-32);
+	}
+	for(i=1;i<strlen(text)-1;i++){
+		if((int)text[i]==95){
+			i++;
+			if((int)text[i]>=97 && (int)text[i]<=122){
+				text[i] =(char)((int)text[i]-32);
+			}
+		}
+	}	
+	printf("%s\n",text);
+	text = NULL;
+
+}
